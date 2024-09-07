d638a6e7ae3be4399482d094fb0c0060efae8f5e
5huv0 <md88748874@gmail.com>
2024-09-08 02:13:42 +0600

    change

--- airbnb-clone-web/src/components/Card.jsx
+++ airbnb-clone-web/src/components/Card.jsx
@@ -11,6 +11,35 @@ export default function Card(){
                 <p><b>From 130$/person</b></p>
             </div>
             </div>
+
+            <div className="card">
+            <img className="card-img-top" src="./src/pics/pic4.png" alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">Katie</h5>
+                <p className="card-text">DO the course with katie.</p>
+                <p><b>From 130$/person</b></p>
+            </div>
+            </div>
+
+
+            <div className="card">
+            <img className="card-img-top" src="./src/pics/pic4.png" alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">Katie</h5>
+                <p className="card-text">DO the course with katie.</p>
+                <p><b>From 130$/person</b></p>
+            </div>
+            </div>
+
+
+            <div className="card">
+            <img className="card-img-top" src="./src/pics/pic4.png" alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">Katie</h5>
+                <p className="card-text">DO the course with katie.</p>
+                <p><b>From 130$/person</b></p>
+            </div>
+            </div>
         </div>
     )
 }
