6045a7667aa5c282d7d66d6642bab057ebb38171
5huv0 <md88748874@gmail.com>
2024-09-08 23:09:39 +0600

    props

--- airbnb-clone-web/src/App.jsx
+++ airbnb-clone-web/src/App.jsx
@@ -9,7 +9,36 @@ function App() {
 
   return (
     <>
-      <Navbar /> <HeadSec /> <Card/>
+      <div className='app'>
+        <Card 
+          img = "./src/pics/pic4.png"
+          name = "Katie"
+          des = "DO the course with katie."
+          price = "From 130$/person"
+        /> 
+
+        <Card 
+          img = "./src/pics/pic5.png"
+          name = "Katier"
+          des = "DO the course with katier."
+          price = "From 135$/person"
+        />
+
+        <Card 
+          img = "./src/pics/pic6.png"
+          name = "Katies"
+          des = "DO the course with katies."
+          price = "From 132$/person"
+        />
+
+        <Card 
+          img = "./src/pics/pic7.png"
+          name = "Katiesa"
+          des = "DO the course with katiesa."
+          price = "From 133$/person"
+        />
+
+      </div>
     </>
   )
 }
--- airbnb-clone-web/src/components/Card.jsx
+++ airbnb-clone-web/src/components/Card.jsx
@@ -1,44 +1,15 @@
 import React from "react";
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className="main-card">
             <div className="card">
-            <img className="card-img-top" src="./src/pics/pic4.png" alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">Katie</h5>
-                <p className="card-text">DO the course with katie.</p>
-                <p><b>From 130$/person</b></p>
-            </div>
-            </div>
-
-            <div className="card">
-            <img className="card-img-top" src="./src/pics/pic5.png" alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">Katie</h5>
-                <p className="card-text">DO the course with katie.</p>
-                <p><b>From 130$/person</b></p>
-            </div>
-            </div>
-
-
-            <div className="card">
-            <img className="card-img-top" src="./src/pics/pic6.png" alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">Katie</h5>
-                <p className="card-text">DO the course with katie.</p>
-                <p><b>From 130$/person</b></p>
-            </div>
-            </div>
-
-
-            <div className="card">
-            <img className="card-img-top" src="./src/pics/pic7.png" alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">Katie</h5>
-                <p className="card-text">DO the course with katie.</p>
-                <p><b>From 130$/person</b></p>
-            </div>
+                <img className="card-img-top" src={props.img}/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.des}</p>
+                    <p><b>{props.price}</b></p>
+                </div>
             </div>
         </div>
     )
