5ff239d146aa4ee683de23992dae7252788ea82f
5huv0 <md88748874@gmail.com>
2024-09-10 01:47:52 +0600

    props

--- airbnb-clone-web/src/App.jsx
+++ airbnb-clone-web/src/App.jsx
@@ -8,65 +8,22 @@ import JokesData from './components/JokesData'
 
 function App() {
 
-  const JokeElement = JokesData.map(joke => {
-    return <Joke joke = {joke.joke} punchline = {joke.punchline} />
-  })
+  // const JokeElement = JokesData.map(joke => {
+  //   return <Joke joke = {joke.joke} punchline = {joke.punchline} />
+  // })
+
+  const colors = [
+    <h3>red</h3>,
+    <h3>green</h3>,
+    <h3>blue</h3>
+  ]
 
   return (
     <>
       <div className='app'>
 
-        {JokeElement}
-
-        {/* <Card 
-          img = "./src/pics/pic4.png"
-          name = "Katie"
-          des = "DO the course with katie."
-          price = "From 130$/person"
-        /> 
-
-        <Card 
-          img = "./src/pics/pic5.png"
-          name = "Katier"
-          des = "DO the course with katier."
-          price = "From 135$/person"
-        />
-
-        <Card 
-          img = "./src/pics/pic6.png"
-          name = "Katies"
-          des = "DO the course with katies."
-          price = "From 132$/person"
-        />
-
-        <Card 
-          img = "./src/pics/pic7.png"
-          name = "Katiesa"
-          des = "DO the course with katiesa."
-          price = "From 133$/person"
-        /> */}
-
-{/* 
-        <Joke 
-          punchline = "Lorem, ipsum dolor."
-        />
-
-        <Joke 
-          joke = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, aperiam!"
-          punchline = "Lorem, ipsum dolor."
-        />
-
-        <Joke 
-          joke = "Lorem ipsum dolor sit amet consectetur."
-          punchline = "Lorem ipsum "
-        />
-
-        <Joke 
-          joke = "Lorem ipsum dolor sit amet consectetur Lorem ornme"
-          punchline = "Lorem ipsum dolor sit amet."
-        /> */}
-
-
+        {colors}
+        {/* {JokeElement} */}
 
       </div>
     </>
