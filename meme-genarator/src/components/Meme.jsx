45d06e883ed5b908fa03f1fc358ef5b91fd9c6c8
5huv0 <md88748874@gmail.com>
2024-10-06 12:39:27 +0600

    meme generator design

--- meme-genarator/src/components/Meme.jsx
+++ meme-genarator/src/components/Meme.jsx
@@ -4,10 +4,15 @@ export default function Meme(){
     return (
         <div className="container main-div">
             <div className="meme-input">
-                <label for="shut-up">Top text</label>
-                <input type="text" placeholder="Shut Up" id="shut-up"/>
-                <label for="take-money">Bottom text</label>
-                <input type="text" placeholder="And take my money" id="take-money"/>
+                <div className="top-txt">
+                    <label for="shut-up">Top text</label>
+                    <input type="text" placeholder="Shut Up" id="shut-up"/>
+                </div>
+                <div className="bottom-txt">
+                    <label for="take-money">Bottom text</label>
+                    <input type="text" placeholder="And take my money" id="take-money"/>
+                </div>
+                
             </div>
             <div className="meme-btn">
                 <button>
