41b5901ddd475ef1230fe2f1def19c67111da506
5huv0 <md88748874@gmail.com>
2024-10-08 08:53:35 +0600

    meme generated

--- meme-genarator/src/components/Meme.jsx
+++ meme-genarator/src/components/Meme.jsx
@@ -3,22 +3,26 @@ import MemeData from "./MemeData";
 
 export default function Meme(){
 
+
+const [getMemeimg, setgetMemeimg] = React.useState("")
+
    function GetMeme(){
-    const MemeArr = MemeData.data.memes;
-    const randomNum = Math.round(Math.random() * MemeArr.length);
-    const url = MemeArr[randomNum].url
-    console.log(url);
+    const MemeArr = MemeData.data.memes; //This will take data from my data file  
+    const randomNum = Math.round(Math.random() * MemeArr.length); //Generated a random number 
+    const url = MemeArr[randomNum].url; //putting url from memedata in MemeArr's random index
+    setgetMemeimg(url);
   }
+
+  
+
 
     return (
         <div className="container main-div">
             <div className="meme-input">
                 <div className="top-txt">
-                    {/* <label htmlFor="shut-up">Top text</label> */}
                     <input type="text" placeholder="Shut Up" id="shut-up"/>
                 </div>
                 <div className="bottom-txt">
-                    {/* <label htmlFor="take-money">Bottom text</label> */}
                     <input type="text" placeholder="And take my money" id="take-money"/>
                 </div>
                 
@@ -27,6 +31,10 @@ export default function Meme(){
                 <button className="meme-btn" onClick={GetMeme}>
                     Get a new Meme image <img src="" alt="" />
                 </button>
+            </div>
+
+            <div className="meme-sec">
+                <img src={getMemeimg} alt="" className="meme-img"/>
             </div>
         </div>
     )
