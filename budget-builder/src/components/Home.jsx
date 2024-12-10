d67e9badd4555c45d8929d192a5de074f91ba371
5huv0 <md88748874@gmail.com>
2024-12-10 16:49:43 +0600

    used react routing also used useState

--- budget-builder/src/components/Home.jsx
+++ budget-builder/src/components/Home.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+
+let [counter, setCounter] = useState(20000)
+
+const addMoney = function add(){
+    setCounter(counter + 10000)
+}
+
+const subMoney = function sub(){
+    setCounter(counter - 5000)
+}
+
   return (
     <div>
-      <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
-        <Link 
-        to='build'>
-            Get the Build
-        </Link>
-      </button>
+        <div>
+            <p className='text-2xl m-9'>
+                Give me your budget here 
+            </p>
+            <p className='text-5xl font-bold'>
+                {counter}
+            </p>
+            <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10' onClick={addMoney}>More </button>
+            <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10' onClick={subMoney}>Less </button>
+        </div>
 
-      <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
-        <Link
-        to='build1'>
-            Get the Build1
-        </Link>
-      </button>
 
-      <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
-        <Link
-        to='build2'>
-            Get the Build2
-        </Link>
-      </button>
+        <div>
+            <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
+                <Link 
+                to='build'>
+                    Get the Build
+                </Link>
+            </button>
+
+            <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
+                <Link
+                to='build1'>
+                    Get the Build1
+                </Link>
+            </button>
+
+            <button className='bg-slate-500 p-8 hover:bg-slate-800 text-white m-10'>
+                <Link
+                to='build2'>
+                    Get the Build2
+                </Link>
+            </button>
+        </div>
+      
     </div>
   )
 }
