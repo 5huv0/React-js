ddbaffb8fc357a1a3f57653be69dc17fb9d3421d
5huv0 <md88748874@gmail.com>
2024-12-14 23:52:01 +0600

    prac

---
+++ currency-converter/src/components/Converter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Converter() {
+  return (
+    <div className='bg-slate-500 p-7 m-9 flex flex-col justify-evenly'>
+        <label htmlFor="">
+            from
+            <input type="text" />
+        </label>
+
+        <button className='bg-red-600 m-5 w-20 h-10'>
+            submit
+        </button>
+
+        <label htmlFor="">
+            to
+            <input type="text" />
+        </label>
+    </div>
+  )
+}
