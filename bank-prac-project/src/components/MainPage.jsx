45ab8151a2dc538d7a9fe52dc6815431b732e7d7
5huv0 <md88748874@gmail.com>
2024-12-15 01:39:05 +0600

    design complete

--- bank-prac-project/src/components/MainPage.jsx
+++ bank-prac-project/src/components/MainPage.jsx
@@ -2,30 +2,40 @@ import React from 'react'
 
 export default function MainPage() {
   return (
-    <div>
-        <div className='grid grid-cols-4 grid-flow-row gap-4 bg-slate-800 m-5'>
-            <div>Deposite</div>
-            <div>Withdraw</div>
-            <div>Balance</div>
+    <div className='text-white text-2xl font-bold'>
+       <div className='flex flex-row justify-evenly m-5 rounded-2xl'>    {/* First div here */}
+        <div className='bg-slate-600 p-16 rounded-2xl'>
+            Deposite 00$
         </div>
-      
-        <div className='grid grid-cols-2 grid-flow-row gap-4 bg-orange-600 m-5 '>
-            <div>
-                <label htmlFor="">
-                    Plz Deposite
-                    <input type="text" />
-                </label>
-                <button className='p-5 bg-black text-white'>Depostie</button>
-            </div>
+        <div className='bg-orange-600 p-16 rounded-2xl'>
+            Withdraw 00$
+        </div>
+        <div className='bg-green-600 p-16 rounded-2xl'>
+            Balance 00$
+        </div>
+       </div>
 
-            <div>
-                <label htmlFor="">
-                    Withdraw
-                    <input type="text" />
-                </label>
-                <button className='p-5 bg-black text-white'>Withdraw</button>
-            </div>
+       <div className='flex flex-row justify-evenly m-5'>    {/* Second div here */}
+        <div className='flex flex-col items-start bg-blue-600 p-16 gap-4 rounded-2xl text-black text-2xl font-bold'>   {/* First input box here */}
+            <label htmlFor=""> 
+                Deposite Money
+            </label>
+            <input type="number" placeholder='Deposite' className='p-2 border-none outline-none'/>
+            <button className='bg-black text-white rounded-3xl p-4' >
+                Deposite
+            </button>
         </div>
+
+        <div className='flex flex-col items-start bg-yellow-400 p-16 gap-4 rounded-2xl text-black text-2xl font-bold'>   {/* second input box here */}
+            <label htmlFor="">
+                Withdraw Money
+            </label>
+            <input type="number" placeholder='Withdraw' className='p-2 outline-none'/>
+            <button className='bg-black text-white rounded-3xl p-4'>
+                Withdraw
+            </button>
+        </div>
+       </div>
     </div>
   )
 }
