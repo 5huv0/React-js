f0ec897c3a66ad6f0856fe9f2fb3fe2a6116561d
5huv0 <md88748874@gmail.com>
2024-12-16 00:03:33 +0600

    practiced use state usecallback

--- bank-prac-project/src/components/MainPage.jsx
+++ bank-prac-project/src/components/MainPage.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 export default function MainPage() {
+    const [deposite, setDeposite] = useState(0)
+    const [withdraw, setWithdraw] = useState(0)
+    const [balance, setBalance] = useState(0)
+
+    const handleDeposite = useCallback(() => {
+        if(deposite > 0){
+            setBalance((prevBalance) => prevBalance + deposite)
+            setDeposite()
+        }
+    },[deposite])
+
+    const handleWithdraw = useCallback(() => {
+        if (withdraw > 0 && withdraw <= balance) {
+          setBalance((prevBalance) => prevBalance - withdraw); // Subtract from balance
+          setWithdraw(''); // Clear the input
+        } 
+        // else if (withdraw > balance) {
+        //   alert('Insufficient balance!');
+        // } 
+        // else {
+        //   alert('Please enter a valid withdrawal amount!');
+        // }
+      }, [withdraw, balance]);
+
   return (
     <div className='text-white text-2xl font-bold'>
        <div className='flex flex-row justify-evenly m-5 rounded-2xl'>    {/* First div here */}
-        <div className='bg-slate-600 p-16 rounded-2xl'>
-            Deposite 00$
+        <div className='bg-slate-600 p-16 rounded-2xl' >
+            Deposite {deposite}$
         </div>
-        <div className='bg-orange-600 p-16 rounded-2xl'>
-            Withdraw 00$
+        <div className='bg-orange-600 p-16 rounded-2xl' >
+            Withdraw {withdraw}$
         </div>
         <div className='bg-green-600 p-16 rounded-2xl'>
-            Balance 00$
+            Balance {balance}$
         </div>
        </div>
 
@@ -20,8 +44,8 @@ export default function MainPage() {
             <label htmlFor=""> 
                 Deposite Money
             </label>
-            <input type="number" placeholder='Deposite' className='p-2 border-none outline-none'/>
-            <button className='bg-black text-white rounded-3xl p-4' >
+            <input type="number" placeholder='Deposite' value={deposite} className='p-2 border-none outline-none' onChange={(e) => setDeposite(e.target.value)}/>
+            <button className='bg-black hover:bg-slate-800 text-white rounded-3xl p-4' onClick={handleDeposite}>
                 Deposite
             </button>
         </div>
@@ -30,8 +54,8 @@ export default function MainPage() {
             <label htmlFor="">
                 Withdraw Money
             </label>
-            <input type="number" placeholder='Withdraw' className='p-2 outline-none'/>
-            <button className='bg-black text-white rounded-3xl p-4'>
+            <input type="number" placeholder='Withdraw' value={withdraw} className='p-2 outline-none' onChange={(e) => setWithdraw(e.target.value)}/>
+            <button className='bg-black hover:bg-slate-800 text-white rounded-3xl p-4' onClick={handleWithdraw}>
                 Withdraw
             </button>
         </div>
@@ -39,3 +63,7 @@ export default function MainPage() {
     </div>
   )
 }
+
+
+
+
