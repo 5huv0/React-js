7e8e66e12cc879a5a7d82d28f9fe65d93bc11e93
5huv0 <md88748874@gmail.com>
2024-12-22 00:25:11 +0600

    currency converter

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import {InputBox} from './components/Index'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+
+  const [amount , setAmount] = useState(0)
+  const [from , setFrom] = useState("usd")
+  const [to , setTo] = useState("bdt")
+  const [convertedAmount , setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
 
   return (
     <>
--- currency-converter/src/hooks/useCurrencyInfo.js
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data , setData] = useState({})
     useEffect(() => {
-        const [data , setData] = useState({})
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]) )
     } , [currency])
-    console.log(data)
     return data
 }
 
